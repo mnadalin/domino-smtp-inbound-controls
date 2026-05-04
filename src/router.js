a830015fe685ef104cb536e08420bd22abbc836d
mnadalin <massimo.nadalin@devangarde.it>
2026-05-04 22:50:27 +0200

    Refactoring

--- src/router.js
+++ src/router.js
@@ -5,9 +5,9 @@ import navConfig from './navConfig.json'
 const items = navConfig.flatMap(group => group.items)
 
 const routes = [
-	{ path: '/', redirect: '/' + items[0].route },
-	...items.map(({ route, ...props }) => ({
-		path: '/' + route,
+	{ path: '/', redirect: items[0].path },
+	...items.map(({ path, ...props }) => ({
+		path,
 		component: ConfigurationItem,
 		props,
 	})),
